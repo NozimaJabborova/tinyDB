b6b45e75005de950f3391e6c248a69007fdb2dbd
Nozima Jabborova <mumtozbegim007@gmail.com>
2021-02-17 10:49:12 +0500

    query

--- read_db.py
+++ read_db.py
@@ -1,8 +1,7 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 db=TinyDB('db.json')
 #db.truncate()
 data=db.all()
-
-for i in data:
-    print(i['user_id'])
-    print(i['username'])
+q=Query()
+user=db.search(q.user_id==3)
+print(user)
